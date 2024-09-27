71c4765c8509a63fdfedb0f4e7cf7c81f9affa71
LucasBerlanda <lucasberlanda02@gmail.com>
2024-09-27 19:56:59 -0300

    commit inicial

---
+++ src/main/java/com/example/multitenancyschema/MultitenancyschemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.multitenancyschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenancyschemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenancyschemaApplication.class, args);
+	}
+
+}
