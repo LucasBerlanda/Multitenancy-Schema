b08f149e9c6388f641c9f1a1bb9d452455672692
LucasBerlanda <lucasberlanda02@gmail.com>
2024-09-27 21:08:00 -0300

    implementado controle multitenancy por schema definido pelo header da request

---
+++ src/main/java/com/example/multitenancyschema/interceptor/TenantInterceptor.java
@@ -0,0 +1,28 @@
+package com.example.multitenancyschema.interceptor;
+
+import com.example.multitenancyschema.multitenancy.TenantContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Component
+public class TenantInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) {
+        Optional.ofNullable(req.getHeader("empresa_id"))
+                .map(String::toUpperCase)
+                .ifPresent(TenantContext::setCurrentTenant);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        TenantContext.clear();
+    }
+
+}
---
+++ src/main/java/com/example/multitenancyschema/multitenancy/TenantContext.java
@@ -0,0 +1,21 @@
+package com.example.multitenancyschema.multitenancy;
+
+public class TenantContext {
+
+    public static final String DEFAULT_SCHEMA = "db_multitenancy";
+
+    private static final ThreadLocal<String> currentTenant = ThreadLocal.withInitial(() -> DEFAULT_SCHEMA);
+
+    public static void setCurrentTenant(String tenant) {
+        currentTenant.set(tenant);
+    }
+
+    public static String getCurrentTenant() {
+        return currentTenant.get();
+    }
+
+    public static void clear() {
+        currentTenant.remove();
+    }
+
+}
